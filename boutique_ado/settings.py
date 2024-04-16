f65a23d8daf859ff790cdebb0a17c490b372a892
Alice Dean <elliott.alice.92@gmail.com>
2024-04-16 17:09:24 +0000

    add product detail page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -156,6 +156,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+'django.contrib.staticfiles.finders.FileSystemFinder',
+)
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
