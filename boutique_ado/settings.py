c016e0862c09e4799124d3e7f98c2708e2198b4c
Alice Dean <elliott.alice.92@gmail.com>
2024-04-26 13:49:46 +0000

    update quantity

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth')
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            os.path.join(BASE_DIR, 'templates', 'includes'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
