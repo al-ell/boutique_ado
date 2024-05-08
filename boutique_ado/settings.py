5a6cd69a39181e35b8394353f9a3c87c09f8edbf
Alice Dean <elliott.alice.92@gmail.com>
2024-05-08 12:00:04 +0000

    add stripe stuff

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
 
+import stripe
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -195,8 +197,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
