f862b506f96662c9e0f0551ed350ba093237dd55
Alice Dean <elliott.alice.92@gmail.com>
2024-05-08 15:07:02 +0000

    add checkout success

--- checkout/admin.py
+++ checkout/admin.py
@@ -20,7 +20,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     fields = ('order_number', 'date', 'full_name',
                 'email', 'phone_number', 'country', 'postcode',
-                'town_or_city', 'street_address1', 'streetadress2',
+                'town_or_city', 'street_address1', 'street_address2',
                 'county', 'delivery_cost', 'order_total', 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
