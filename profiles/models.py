a5b10a557840c11da53f1fa0cbdc089a2f1a46fa
Alice Dean <elliott.alice.92@gmail.com>
2024-05-10 06:59:10 +0000

    profile page update

--- profiles/models.py
+++ profiles/models.py
@@ -8,9 +8,9 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
 
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label="Country *", null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -24,6 +24,6 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.jobjects.create(user=instance)
+        UserProfile.objects.create(user=instance)
     
     instance.userprofile.save()
