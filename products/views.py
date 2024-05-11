28cfe4c7638203f6dfbd4a6ced2d909d5ca0674e
Alice Dean <elliott.alice.92@gmail.com>
2024-05-11 18:00:52 +0000

    add edit for superusers

--- products/views.py
+++ products/views.py
@@ -80,9 +80,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure form is valid.')
     else:
@@ -97,10 +97,16 @@ def add_product(request):
     return render(request, template, context)
 
 def edit_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
-        product = get_object_or_404(Product, pk=product_id)
-        if request.method == 'POST':
-            form = ProductForm(request.POST, request.FILES, instance=product)
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully edited product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, f'Failed to update product. please ensure form is valid.')
+    else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
        
@@ -111,3 +117,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product Deleted!')
+
+    return redirect(reverse('products'))
+
+
