67698df6e8921e4c7c0b3ac058d935ec5c0e9894
Alice Dean <elliott.alice.92@gmail.com>
2024-05-11 14:52:12 +0000

    edit products

--- products/views.py
+++ products/views.py
@@ -95,3 +95,19 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+def edit_product(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES, instance=product)
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+       
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
