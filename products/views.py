781d5887e631659719794a40c4308aa41cc314c4
Alice Dean <elliott.alice.92@gmail.com>
2024-05-13 10:36:59 +0000

    product admin complete

--- products/views.py
+++ products/views.py
@@ -76,9 +76,9 @@ def product_detail(request, product_id):
 
 @login_required
 def add_product(request):
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only admin can access this page')
-        return redirect(reverse('home'))
+    # if not request.user.is_superuser:
+    #     messages.error(request, 'Sorry, only admin can access this page')
+    #     return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -102,9 +102,9 @@ def add_product(request):
 
 @login_required
 def edit_product(request, product_id):
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only admin can access this page')
-        return redirect(reverse('home'))
+    # if not request.user.is_superuser:
+    #     messages.error(request, 'Sorry, only admin can access this page')
+    #     return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -129,9 +129,9 @@ def edit_product(request, product_id):
 
 @login_required
 def delete_product(request, product_id):
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only admin can access this page')
-        return redirect(reverse('home'))
+    # if not request.user.is_superuser:
+    #     messages.error(request, 'Sorry, only admin can access this page')
+    #     return redirect(reverse('home'))
         
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
