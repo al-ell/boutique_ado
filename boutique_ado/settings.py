c7f480634797c341ddf498ad779ff668b5bf8bb3
Alice Dean <elliott.alice.92@gmail.com>
2024-05-29 09:58:32 +0000

    update secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tg!^o=p7k0=h8%pgem!wbmn(g1_jkqo_3d)pe0nx$-#lhap^vd'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-alell-shopwalkthrough-tgqg2unz19i.ws-eu111.gitpod.io',
